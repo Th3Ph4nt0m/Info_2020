bcde80b8c2baa327e290bae074e9858239224eb0
Henrik S <Development@th3ph4nt0m.de>
2020-08-27 15:37:45 +0200

    - commit of lesson on 27/08/2020

--- src/main/java/de/th3ph4nt0m/info/wuerfel/Wuerfelspiel.java
+++ src/main/java/de/th3ph4nt0m/info/wuerfel/Wuerfelspiel.java
@@ -4,24 +4,30 @@
  * Wuerfelspiel.java is part of the Info_2020 project.
  * Info_2020 is licensed under the MIT license.
  *
- * last edit: 2020/8/25
+ * last edit: 2020/8/27
  */
 
 package de.th3ph4nt0m.info.wuerfel;
 
+@SuppressWarnings("ALL")
 public class Wuerfelspiel {
 
     Wuerfel meinWuerfel = new Wuerfel();
     private int augensumme;
+    private int[] history = new int[6];
 
     public Wuerfelspiel() {
 
     }
 
     public void sechsMalWuerfeln() {
+        augensumme = 0;
         for (int i = 0; i < 5; i++) {
             meinWuerfel.wuerfeln();
+            augensumme = augensumme + meinWuerfel.getAugenzahl();
+            history[i] = meinWuerfel.getAugenzahl();
         }
+        System.out.println(history);
     }
 
     public void printAugensumme() {
