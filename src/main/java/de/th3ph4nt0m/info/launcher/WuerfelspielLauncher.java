20b3e2e171235bdac720dd84a254c74b7fefba5a
Henrik S <Development@th3ph4nt0m.de>
2020-09-01 23:21:19 +0200

    - commit of lesson on 01/09/2020

---
+++ src/main/java/de/th3ph4nt0m/info/launcher/WuerfelspielLauncher.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2020 Henrik Steffens aka. Th3Ph4nt0m
+ *
+ * WuerfelspielLauncher.java is part of the Info_2020 project.
+ * Info_2020 is licensed under the MIT license.
+ *
+ * last edit: 2020/9/1
+ */
+
+package de.th3ph4nt0m.info.launcher;
+
+public class WuerfelspielLauncher {
+
+    public static void main(String[] s) {
+
+    }
+}
