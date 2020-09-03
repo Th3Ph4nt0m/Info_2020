cbad45782c87d00c57d3f105fa5605396bd9e187
Henrik S <Development@th3ph4nt0m.de>
2020-09-03 13:57:42 +0200

    - added buildArray method

--- src/main/java/de/th3ph4nt0m/info/wuerfel/Wuerfel.java
+++ src/main/java/de/th3ph4nt0m/info/wuerfel/Wuerfel.java
@@ -4,7 +4,7 @@
  * Wuerfel.java is part of the Info_2020 project.
  * Info_2020 is licensed under the MIT license.
  *
- * last edit: 2020/9/1
+ * last edit: 2020/9/3
  */
 
 package de.th3ph4nt0m.info.wuerfel;
@@ -19,12 +19,30 @@ public class Wuerfel {
     private int augenzahl;
 
     //rolls the dice --> returns a random number between 1 and 6
-    public int wuerfeln() {
+    public int wuerfeln()
+    {
         return utils.getRandom(1, 6);
     }
 
     //Getter methods
-    public int getAugenzahl() {
+    public int getAugenzahl()
+    {
         return augenzahl;
     }
+
+
+    //builds an array with a specific length
+    public int[] buildArray(int lenth)
+    {
+        //declare the array
+        int[] array = new int[lenth];
+        for (int i = 0; lenth < array.length; i++) {
+            //initialize every int in the array with 1
+            array[i] = 1;
+        }
+        //returns the completed array
+        return array;
+    }
+
+
 }
