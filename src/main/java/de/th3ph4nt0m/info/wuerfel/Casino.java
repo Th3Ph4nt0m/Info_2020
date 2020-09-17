fdbc23be9a8f3968f166b22998c964f151b33318
Henrik S <Development@th3ph4nt0m.de>
2020-09-17 12:57:50 +0200

    - added 2D-Array to Casino.java

--- src/main/java/de/th3ph4nt0m/info/wuerfel/Casino.java
+++ src/main/java/de/th3ph4nt0m/info/wuerfel/Casino.java
@@ -4,24 +4,26 @@
  * Casino.java is part of the Info_2020 project.
  * Info_2020 is licensed under the MIT license.
  *
- * last edit: 2020/9/15
+ * last edit: 2020/9/17
  */
 
 package de.th3ph4nt0m.info.wuerfel;
 
 import de.th3ph4nt0m.info.utils.Utils;
 
-@SuppressWarnings({"ForLoopReplaceableByForEach", "unused", "RedundantSuppression"})
+@SuppressWarnings({"ForLoopReplaceableByForEach", "unused", "RedundantSuppression", "FieldCanBeLocal", "FieldMayBeFinal"})
 public class Casino
 {
-    private final Wuerfelspiel[] spielTische;
     //initializing attributes/objects/variables
     Utils utils = new Utils();
+    private final Wuerfelspiel[] spielTische;
+    private int[][] tischAugensumme;
 
     //Constructor
     public Casino(int pSpielAnzahl)
     {
         spielTische = new Wuerfelspiel[pSpielAnzahl];
+        tischAugensumme = new int[2][pSpielAnzahl];
         erzeugeSpieltische();
     }
 
@@ -33,17 +35,22 @@ public class Casino
         }
     }
 
-    //excecute Wuerfelspiel#sechsMalWuerfeln for every object in the array
+
     public void spieleStarten()
     {
         for (int i = 0; i < spielTische.length; i++) {
+            //excecute Wuerfelspiel#sechsMalWuerfeln for every object in the array
             spielTische[i].sechsMalWuerfeln();
+            //saving the table number and the result of each table in 2D-Array
+            tischAugensumme[0][i] = i + 1; //saving the table number, not the index in line 1
+            tischAugensumme[1][i] = spielTische[i].getAugensumme();
         }
     }
 
     //print the "results" of the single objects and the winner
     public void spieltischeAusgeben()
     {
+        //TODO tischAugensumme speichert Tischzahl und Ergebnis, gibt diese danach aus
         int[] ii = new int[spielTische.length];
         for (int i = 0; i < spielTische.length; i++) {
             ii[i] = spielTische[i].getAugensumme();
