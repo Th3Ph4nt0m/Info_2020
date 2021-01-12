369ddc44828f4d0f0e3aa4883a012466b313f407
Henrik Steffens <Development@th3ph4nt0m.de>
2021-01-12 14:10:10 +0100

    implemented weird-named class

---
+++ src/main/java/de/th3ph4nt0m/info/trhekegmu/Trhekegmu.java
@@ -0,0 +1,52 @@
+package de.th3ph4nt0m.info.trhekegmu;
+
+
+import de.th3ph4nt0m.info.abi.Queue;
+import de.th3ph4nt0m.info.abi.Stack;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Trhekegmu {
+
+    private static final Queue<Integer> q = new Queue<>();
+    private static final Stack<Integer> s = new Stack<>();
+
+    public static void main(String[] args) {
+        execute("10,4,6,8,1,3,2");
+        printNums();
+    }
+
+    public static void execute(String text) {
+        List<String> list = Arrays.asList(text.split(","));
+
+        int i = 0;
+
+        while (!list.isEmpty()) {
+            if (list.size() != i) {
+                q.enqueue(Integer.parseInt(list.get(i)));
+
+                i++;
+            } else {
+                break;
+            }
+        }
+
+        while (!q.isEmpty()) {
+            s.push(q.front());
+            q.dequeue();
+        }
+    }
+
+    public static void printNums() {
+        String txt = "";
+
+        while (!s.isEmpty()) {
+            txt = txt + s.top() + "\n";
+            s.pop();
+        }
+
+        System.out.println(txt);
+
+    }
+}
