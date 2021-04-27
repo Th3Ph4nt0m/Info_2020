dd880a62b78d1ea12b42b339089f87122e7ad15e
Henrik Steffens <Development@th3ph4nt0m.de>
2021-04-27 16:19:14 +0200

    add cardgame module

---
+++ CardGame/src/main/java/de/th3ph4nt0m/info/cardgame/Cardgame.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2021 Henrik Steffens
+ *
+ * Cardgame.java is part of the Info_2020 project.
+ * Info_2020 is licensed under the MIT license.
+ */
+
+package de.th3ph4nt0m.info.cardgame;
+
+import de.th3ph4nt0m.info.cardgame.model.Card;
+
+import java.util.Random;
+
+public class Cardgame {
+    Card[] cards;
+
+    /**
+     * Creates a "Skat" card deck (32 cards).
+     */
+    public Cardgame() {
+        this.cards = new Card[32];
+        for (Card.Value value : Card.Value.values()) {
+            for (Card.Face face : Card.Face.values()) {
+                for (int i = 0; i < cards.length; i++) {
+                    cards[i] = new Card(value, face);
+                }
+            }
+        }
+    }
+
+    /**
+     * Shuffles the whole card deck
+     * @param turns Number of turns the deck is to be shuffled
+     */
+    public void shuffle(int turns){
+        Random rand = new Random();
+        for (int i = 0; i < turns; i++) {
+            int index1 = rand.nextInt(cards.length);
+            int index2 = rand.nextInt(cards.length);
+            swap(index1, index2);
+        }
+    }
+
+    /**
+     * Swaps two cards positions in the array
+     * @param index1 Index of the first card
+     * @param index2 Index of the second card
+     */
+    private void swap(int index1, int index2){
+        Card card1 = cards[index1];
+        Card card2 = cards[index2];
+
+        cards[index1] = card2;
+        cards[index2] = card1;
+    }
+}
