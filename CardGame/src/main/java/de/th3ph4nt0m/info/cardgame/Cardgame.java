260ad370f1ecb7d42d1617bbf69e0733a48a971d
Henrik Steffens <Development@th3ph4nt0m.de>
2021-05-10 18:32:46 +0200

    implement 2 methods

--- CardGame/src/main/java/de/th3ph4nt0m/info/cardgame/Cardgame.java
+++ CardGame/src/main/java/de/th3ph4nt0m/info/cardgame/Cardgame.java
@@ -30,9 +30,10 @@ public class Cardgame {
 
     /**
      * Shuffles the whole card deck
+     *
      * @param turns Number of turns the deck is to be shuffled
      */
-    public void shuffle(int turns){
+    public void shuffle(int turns) {
         Random rand = new Random();
         for (int i = 0; i < turns; i++) {
             int index1 = rand.nextInt(cards.length);
@@ -43,10 +44,11 @@ public class Cardgame {
 
     /**
      * Swaps two cards positions in the array
+     *
      * @param index1 Index of the first card
      * @param index2 Index of the second card
      */
-    private void swap(int index1, int index2){
+    private void swap(int index1, int index2) {
         Card card1 = cards[index1];
         Card card2 = cards[index2];
 
@@ -56,16 +58,54 @@ public class Cardgame {
 
     /**
      * Checks if the given card exists in the deck
-     * @param face Card face as a string
+     *
+     * @param face  Card face as a string
      * @param value Card value as a string
      * @return true if the card exists | false if the card not exists
      */
-    public boolean exists(String face, String value){
+    public boolean exists(String face, String value) {
         for (Card card : cards) {
             if (card.getFace().toString().equalsIgnoreCase(face) && card.toString().equalsIgnoreCase(value)) {
                 return true;
             }
         }
         return false;
+    }
+
+    /**
+     * Removes all cards of a given value
+     *
+     * @param value Value of the cards to be removed
+     */
+    public void remove(String value) {
+        for (int i = 0; i < cards.length; i++) {
+            if (cards[i].getValue().toString().equalsIgnoreCase(value)) {
+                cards[i] = null;
+            }
+        }
+    }
+
+    /**
+     * Calculates the distribution of the suits in the first half of the deck
+     *
+     * @return 0 if red amount equals black amount | 1 if black amount is higher than red amount | 2 if black amount is lower than red amount
+     */
+    public int countFirstHalf() {
+        int counter = 0;
+        for (int i = 0, cardsLength = 16; i < cardsLength; i++) {
+            Card card = cards[i];
+            if (card.getValue().equals(Card.Value.CLUB) || card.getValue().equals(Card.Value.SPADE)) {
+                counter++;
+            }
+        }
+        int result = 16 - counter;
+
+        if (result < counter) {
+            return 2;
+        } else if (result > counter) {
+            return 1;
+        } else {
+            return 0;
+        }
     }
 }
